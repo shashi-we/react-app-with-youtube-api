f35a1f903385ec3f38982313be6b4e7c3263e350
Shashi Singh <shashi.we@gmail.com>
2017-10-03 07:51:04 +0530

    use constructor and state in component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
   render() {
-    return <input onChange={this.onInputChange} />;
+    return (
+      <div>
+        <input
+          value={ this.state.term }
+          onChange={ this.onInputChange } />
+      </div>
+    );
   }
 
   onInputChange(e) {
-    console.log(e.target.value);
+    this.setState({
+      term : e.target.value
+    });
   }
 }
 
